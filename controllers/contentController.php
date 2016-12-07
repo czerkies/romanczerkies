2fd15b91aa482d0637f69af77d896e9959c70be0
czerkies <roman.czerkies@gmail.com>
2016-12-07 20:02:21 +0100

    Feat(/): MAJ MVC
    MAJ commentaire et code du mvc
    
    Issues #1

--- controllers/contentController.php
+++ controllers/contentController.php
@@ -6,11 +6,30 @@ class contentController extends superController {
 
     $meta['file_name'] = 'home';
 
-    $hello = 'Hello World';
+    $hub = [
+      'Twitter' => [
+        'href' => 'https://twitter.com/roman_czerkies',
+        'title' => '@roman_czerkies',
+      ],
+      'LinkedIn' => [
+        'href' => 'linkedin',
+        'title' => 'Voir le profil professionnel de Roman Czerkies sur LinkedIn',
+      ],
+      'GitHub' => [
+        'href' => 'github',
+        'title' => 'Follow their code on GitHub',
+      ],
+      'Strava' => [
+        'href' => 'strava',
+        'title' => 'Cyclist on Strava',
+      ]
+    ];
+
+    $functions = new functionsController();
 
     $this->render(
       $meta,
-      ['hello' => $hello]
+      ['hub' => $hub, 'functions' => $functions]
     );
 
   }
--- models/publicModel.php
+++ models/publicModel.php
@@ -6,7 +6,7 @@
  *
  * @version v10.0.0
  * @link http://romanczerkies.fr/
- * @since v10.0.0-alpha.1
+ * @since v10.0.0-alpha
  */
 class publicModel extends superModel {
 
