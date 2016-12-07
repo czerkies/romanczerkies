a1149b9fdcb25152d4149eb7601d963ca15d6d62
czerkies <roman.czerkies@gmail.com>
2016-12-07 20:03:10 +0100

    Feat(home.php): Template home.php
    Création du template home.php avec fonction formulaire.
    
    Issues #4

--- views/content/home.php
+++ views/content/home.php
@@ -1,1 +1,19 @@
-<?= $hello; ?>
+<nav>
+  <ul>
+    <?php foreach ($hub as $key => $val) : ?><li>
+      <a href="<?= $val['href']; ?>" title="<?= $val['title']; ?>"><?= $key; ?></a>
+    </li>
+    <?php endforeach; ?><li>
+      <label for="1">Contact</label>
+    </li>
+  </ul>
+</nav>
+<form method="post">
+  <?php $functions->fieldsFormInput(1); ?>
+  <div>
+    <?php $functions->fieldsFormTextarea(2, TRUE, TRUE); ?>
+  </div>
+  <div>
+    <?php $functions->fieldsFormInput('', 'submit', FALSE, FALSE, 'Envoyer'); ?>
+  </div>
+</form>
