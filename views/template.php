1d55f8f5a1e4c77d651eb582151492acfacef50d
czerkies <roman.czerkies@gmail.com>
2016-12-08 08:03:50 +0100

    Feat(template.php): Mise en place sémantique html
    Sémantique html et intégration du buffer.
    
    Issues #3

--- views/template.php
+++ views/template.php
@@ -1,10 +1,18 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
   <head>
     <meta charset="utf-8">
+    <meta name="description" content="<?= $meta['desription']; ?>">
     <title><?= $meta['title']; ?></title>
+    <link rel="stylesheet" href="<?= RACINE; ?>css/style.css">
   </head>
   <body>
+    <header>
+      <a href="<?= RACINE; ?>" title="Accueil">
+        <h1><?= $meta['title']; ?></h1>
+        <h2>Dev FT FT</h2>
+      </a>
+    </header>
     <?= $buffer; ?>
   </body>
 </html>
