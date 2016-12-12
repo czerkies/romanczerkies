249face7b473c21ba9bdb72c03d3a8634f96099e
czerkies <roman.czerkies@gmail.com>
2016-12-12 18:49:58 +0100

    Feat(controllers/functionController.php): MAJ des method
    MAJ des method pour récupération des datas.
    Fonction pour récupération du POST.
    MAJ des génération de fields form.
    
    Issues #2

--- controllers/functionsController.php
+++ controllers/functionsController.php
@@ -27,19 +27,20 @@ class functionsController extends superController {
       $field .= '<input type="' . $type . '"';
       if ($name) $field .= ' name="' . $name . '"';
       if ($id) $field .= ' id="' . $name . '"';
-      if ($attr) foreach ($attr as $key => $value) $field .= ' ' . $key . '="' . $value . '"';
-      if ($_POST && isset($_POST[$name])) $field .= ' value="' . $_POST[$name] . '"';
+      if ($attr) foreach ($attr as $key => $value) if(!empty($val)) $field .= ' ' . $key . '="' . $value . '"';
+      if (isset($_POST[$name]) && !empty($_POST[$name])) $field .= ' value="' . $_POST[$name] . '"';
       elseif ($value) $field .=  ' value="' . $value . '"';
       if ($required) $field .= ' required';
       $field .= ">\n";
 
+      echo $field;
+
     } else {
 
-      $field .= 'Attribuez un nom à votre champs.';
+      self::displayError(__CLASS__, __FUNCTION__, "Attribuez un nom à votre champs.");
 
     }
 
-    echo $field;
 
   }
 
@@ -57,16 +58,17 @@ class functionsController extends superController {
   *
   * @return $field string
   */
-  public function fieldsFormTextarea($name = '', $id = FALSE, $required = FALSE, $value = '', $attr = array()) {
+  public function fieldsFormTextarea($name = '', $id = FALSE, $required = FALSE, $value = NULL, $attr = array()) {
 
     $field = '';
 
     $field .= '<textarea name="' . $name . '"';
     if ($id) $field .= ' id="' . $name . '"';
-    if ($attr) foreach ($attr as $key => $value) $field .= ' ' . $key . '="' . $value . '"';
+    if ($attr) foreach ($attr as $key => $val) if(!empty($val)) $field .= ' ' . $key . '="' . $val . '"';
     if ($required) $field .= ' required';
     $field .= '>';
-    if ($_POST && $_POST[$name]) $field .= $_POST[$name];
+    if(!$attr['placeholder']) if (isset($_POST[$name]) && !empty($_POST[$name])) $field .= $_POST[$name];
+    elseif ($value) $field .= $value;
     $field .= "</textarea>\n";
 
     echo $field;
@@ -83,14 +85,52 @@ class functionsController extends superController {
   * @param (string) $from (option)
   *
   */
-  public function sendMail($to, $subject, $content, $from = EMAIL){
+  public function sendMail($to, $subject, $content, $from = EMAIL) {
 
     $headers = 'Content-Type: text/html; charset=\"UTF-8\";' . "\r\n";
-    $headers .= 'FROM: Le petit St Bernard <' . $from . '>' . "\r\n";
+    $headers .= 'FROM: Roman Czerkies <' . $from . '>' . "\r\n";
 
-    $subjectFormat = "Le petit St bernard - " . $subject;
+    $subjectFormat = "Roman Czerkies - " . $subject;
 
     mail($to, $subjectFormat, $content, $headers);
+
+  }
+
+  /**
+  * Fonction traitement post
+  *
+  *
+  */
+  public function contactPost() {
+
+    if(!empty($_POST[1]) && empty($_POST[2])) {
+
+      $message = htmlentities($_POST[1], ENT_QUOTES);
+
+      $words = explode(' ', trim($message, "\t\n\r\0\x0B"));
+      /*echo "<pre>";
+      var_dump($words);*/
+
+      $emails = [];
+      $urls = [];
+
+      foreach ($words as $value) {
+
+        if(filter_var($value, FILTER_VALIDATE_EMAIL)) $emails[] = $value;
+        if(filter_var($value, FILTER_VALIDATE_URL)) $urls[] = $value;
+
+      }
+
+      //$return = array_key_exists($message, $datas) ? $datas[$message] : "Votre message a bien été envoyé."; // @TODO uppercase
+      $return = "Votre message a bien été envoyé.";
+
+    } else {
+
+      $return = "Votre message n'a pas été envoyé.";
+
+    }
+
+    return $return;
 
   }
 
