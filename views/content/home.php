c47879d7b0ffebaa276b080f86182a67886aa958
czerkies <roman.czerkies@gmail.com>
2016-12-29 19:00:19 +0100

    Feat(home.php): Ajout information et sécurité.
    Ajout du target _blank dans les liens RS
    Ajout du title sur le label contact
    Ajout minlength en attr sur le textarea
    
    Issues #4

--- views/content/home.php
+++ views/content/home.php
@@ -1,17 +1,17 @@
     <nav>
       <ul>
         <?php foreach ($hub as $val): ?><li>
-          <a href="//<?= $val['href']; ?>" title="<?= $val['title']; ?>"><?= $val['value']; ?></a>
+          <a href="//<?= $val['href']; ?>" title="<?= $val['title']; ?>" target="_blank"><?= $val['value']; ?></a>
         </li>
         <?php endforeach; ?><li>
-          <label for="1">Contact</label>
+          <label for="1" title="Keep in touch">Contact</label>
         </li>
       </ul>
     </nav>
     <form method="post">
       <?php $functions->fieldsFormInput(2); ?>
       <div>
-        <?php $functions->fieldsFormTextarea(1, TRUE, TRUE, FALSE, array('placeholder' => $post)); ?>
+        <?php $functions->fieldsFormTextarea(1, TRUE, TRUE, FALSE, array('minlength' => 5, 'placeholder' => $post)); ?>
       </div>
       <div>
         <?php $functions->fieldsFormInput('', 'submit', FALSE, FALSE, 'Envoyer'); ?>
