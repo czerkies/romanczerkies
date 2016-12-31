22aea6996ff5196685d6f3a8c2957d15365281f2
roman <roman.czerkies@gmail.com>
2016-12-31 11:40:06 +0100

    Feat(template.php): Import css
    Import CSS pour amélioration de la performance.
    Gestion d'affichage d'HOTJAR via params.php
    
    Issues #3 #9

--- views/template.php
+++ views/template.php
@@ -4,7 +4,9 @@
     <meta charset="utf-8">
     <meta name="description" content="<?= $meta['description']; ?>">
     <title><?= $meta['title']; ?></title>
-    <link rel="stylesheet" href="<?= RACINE; ?>css/style.css">
+    <style type="text/css">
+      <?php include RACINE_SERVER . RACINE . 'css/style.css'; ?>
+    </style>
   </head>
   <body>
     <header>
@@ -15,15 +17,17 @@
     </header>
 <?= $buffer; ?>
     <!-- romanczerkies.fr Version 10.0. ©<?= date('Y'); ?> Roman Czerkies. Tous droits réservés. - SIRET 79321556700017-->
-    <script>
-        (function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:103348,hjsv:5};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-        })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+<?php if (HOTJAR): ?>
+    <script type="text/javascript">
+      (function(h,o,t,j,a,r){
+          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+          h._hjSettings={hjid:103348,hjsv:5};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+      })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
     </script>
+<?php endif; ?>
   </body>
 </html>
