72046df3da9236507796d71f8926154b50c507bc
roman <roman.czerkies@gmail.com>
2016-12-31 10:22:17 +0100

    Feat(template.php): Hotjar et versioning
    Mise en place d'hotjar.
    Versioning commentaire html.
    
    Issues #4

--- views/template.php
+++ views/template.php
@@ -14,6 +14,16 @@
       </a>
     </header>
 <?= $buffer; ?>
-  <!-- romanczerkies.fr Version 10.0 -->
+    <!-- romanczerkies.fr Version 10.0. ©<?= date('Y'); ?> Roman Czerkies. Tous droits réservés. - SIRET 79321556700017-->
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:103348,hjsv:5};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
   </body>
 </html>
