93870adc51756c67b5d77cf0f8a9e173c5c6edf3
roman <roman.czerkies@gmail.com>
2016-12-31 10:23:49 +0100

    Feat(/www/): Référencement
    Mise en place du robots.txt
    Sitemap.php dynamique.
    
    Issues #9

---
+++ www/sitemap.php
@@ -0,0 +1,26 @@
+<?php
+
+  header('Content-type: application/xml');
+
+  include '../conf/params.php';
+  include '../controllers/superController.php';
+  include '../models/superModel.php';
+
+  $model = new superModel();
+  $sql = "SELECT url FROM pages WHERE restriction = 0 AND url IS NOT NULL";
+  $datas = $model->pdo()->query($sql);
+
+  echo '<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+
+?>
+<url>
+  <loc>http://<?= $_SERVER['SERVER_NAME'] . RACINE; ?></loc>
+  <priority>1</priority>
+</url>
+<?php if($datas->fetchAll()) foreach ($datas->fetchAll() as $value) { ?>
+  <url>
+    <loc>http://<?= $_SERVER['SERVER_NAME'] . $value['url']; ?></loc>
+  </url>
+<?php } ?>
+</urlset>
