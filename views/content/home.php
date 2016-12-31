14ab159c0292e9e37060619a64ad50271032f0ee
czerkies <roman.czerkies@gmail.com>
2016-12-31 18:09:27 +0100

    Feat(/views/): Ajout title
    Amélioration et ajout title.
    
    Issues #11

--- views/content/home.php
+++ views/content/home.php
@@ -1,19 +1,19 @@
     <nav>
       <ul>
-        <?php foreach ($hub as $val): ?><li>
+        <?php if ($hub) foreach ($hub as $val): ?><li>
           <a href="//<?= $val['href']; ?>" title="<?= $val['title']; ?>" target="_blank"><?= $val['value']; ?></a>
         </li>
         <?php endforeach; ?><li>
-          <label for="1" title="Keep in touch">Contact</label>
+          <label for="1" title="Contacter">Contact</label>
         </li>
       </ul>
     </nav>
     <form method="post">
       <?php $functions->fieldsFormInput(2); ?>
       <div>
-        <?php $functions->fieldsFormTextarea(1, TRUE, TRUE, FALSE, array('minlength' => 5, 'placeholder' => $post)); ?>
+        <?php $functions->fieldsFormTextarea(1, TRUE, TRUE, FALSE, array('minlength' => 5, 'title' => 'Votre message', 'placeholder' => $post)); ?>
       </div>
       <div>
-        <?php $functions->fieldsFormInput('', 'submit', FALSE, FALSE, 'Envoyer'); ?>
+        <?php $functions->fieldsFormInput('', 'submit', FALSE, FALSE, 'Envoyer', array('title' => 'Envoyer votre message')); ?>
       </div>
     </form>
