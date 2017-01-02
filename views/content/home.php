b9d737f41b2ee0b84b69000215910e16f608e1e1
czerkies <roman.czerkies@gmail.com>
2017-01-02 09:01:16 +0100

    Fix(home.php): Sémantique html
    Debug de la sémantique html front /views
    
    Issues #11

--- views/content/home.php
+++ views/content/home.php
@@ -1,9 +1,11 @@
     <nav>
       <ul>
-        <?php if ($hub) foreach ($hub as $val): ?><li>
+<?php if ($hub) foreach ($hub as $val): ?>
+        <li>
           <a href="//<?= $val['href']; ?>" title="<?= $val['title']; ?>" target="_blank"><?= $val['value']; ?></a>
         </li>
-        <?php endforeach; ?><li>
+<?php endforeach; ?>
+        <li>
           <label for="1" title="Contacter">Contact</label>
         </li>
       </ul>
