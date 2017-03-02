b8086224237b87698e91640f1f223856518abf55
czerkies <roman.czerkies@gmail.com>
2017-03-02 19:11:14 +0100

    Feat(gulpfile.js): MAJ du gulpfile.js
    Nouvelle fonction watcher sans compréssion.
    
    Issues #13

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,16 @@ var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
   return gulp.src('www/css/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sass({outputStyle: ''}).on('error', sass.logError))
     .pipe(gulp.dest('www/css/'));
 });
 
 gulp.task('watch:sass', function () {
   gulp.watch('www/css/*.scss', ['sass']);
+});
+
+gulp.task('sass:compressed', function() {
+  return gulp.src('www/css/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('www/css/'));
 });
