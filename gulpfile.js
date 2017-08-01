2a894ecbc913f7562a9643c418ca48dbba053f01
czerkies <roman.czerkies@gmail.com>
2017-08-01 21:40:15 +0200

    Feat(gulpfile.js): Màj gulpfile.js
    Ajout de constante et màj task.
    
    Issues #16

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,21 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-const sassfiles = 'www/css/**/*.scss';
+const sassFiles = 'www/css/**/*.scss';
+const sassDest = 'wwww/css';
 
 gulp.task('sass', function () {
-  return gulp.src(sassfiles)
+  return gulp.src(sassFiles)
     .pipe(sass({outputStyle: ''}).on('error', sass.logError))
-    .pipe(gulp.dest('www/css/'));
+    .pipe(gulp.dest(sassDest));
 });
 
 gulp.task('watch:sass', function () {
-  gulp.watch(sassfiles, ['sass']);
+  gulp.watch(sassFiles, ['sass']);
 });
 
 gulp.task('compressed:sass', function() {
-  return gulp.src(sassfiles)
+  return gulp.src(sassFiles)
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('www/css/'));
+    .pipe(gulp.dest(sassDest));
 });
